aa4063dc4904b6e65172a7d9832ea75aad7b070f
taishikato <taishi.k0903@gmail.com>
2020-04-19 19:31:18 -0700

    :zap: set location as asia-northeast1

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,8 @@
 import * as functions from 'firebase-functions';
 
-exports.scheduledStreakSetterCrontab = functions.pubsub
-  .schedule('0 0 * * *')
+exports.scheduledStreakSetterCrontab = functions
+  .region('asia-northeast1')
+  .pubsub.schedule('0 0 * * *')
   .timeZone('Asia/Tokyo') // Users can choose timezone - default is America/Los_Angeles
   .onRun((context) => {
     console.log('This will be run every day at 00:00 AM Asia/Tokyo!');
